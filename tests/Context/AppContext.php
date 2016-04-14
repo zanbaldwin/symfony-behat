1d31e2a2e7afc55c60dee74aead77ef54f8a1cb4
Zander Baldwin <hello+github@zanderbaldwin.com>
2016-04-14 15:35:13 +0100

    Update AppContext

--- tests/Context/AppContext.php
+++ tests/Context/AppContext.php
@@ -8,14 +8,15 @@ use Behat\MinkExtension\Context\MinkContext;
 class AppContext extends MinkContext implements SnippetAcceptingContext
 {
     /**
+     * The MinkContext provides the step "I should be on the homepage", but not a step for "I should not be on the
+     * homepage". Create that step here and change "addressEquals" to "addressNotEquals". To see the original step
+     * this was copied from, see the method "Behat\MinkExtension\Context\MinkContext::assertHomepage" from the
+     * "behat/mink-extension" package.
+     *
      * @Then I should be on the homepage
      */
     public function assertNotHomepage()
     {
-        // The MinkContext provides the step "I should be on the homepage", but not a step for "I should not be on the
-        // homepage". Create that step here and change "addressEquals" to "addressNotEquals". To see the original step
-        // this was copied from, see the method "Behat\MinkExtension\Context\MinkContext::assertHomepage" from the
-        // "behat/mink-extension" package.
         $this->assertSession()->addressNotEquals($this->locatePath('/'));
     }
     
